933c6f6a0967b30e8495a2ae5891bcc5e0da5783
4Sighteducation <admin@vespa.academy>
2025-11-09 22:33:53 +0000

    Version 1k - View Answers modal, repositioned buttons, ultra-compact print (1-2 pages)

--- individual-report/vite.config.js
+++ individual-report/vite.config.js
@@ -8,11 +8,11 @@ export default defineConfig({
     rollupOptions: {
       input: './index.html',
       output: {
-        entryFileNames: 'report1j.js',
+        entryFileNames: 'report1k.js',
         chunkFileNames: '[name]-[hash].js',
         assetFileNames: (assetInfo) => {
           if (assetInfo.name?.endsWith('.css')) {
-            return 'report1j.css'
+            return 'report1k.css'
           }
           return '[name]-[hash][extname]'
         }
